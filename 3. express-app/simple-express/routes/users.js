3c64545691497a5e308503e515a3c2252ee1ecee
Dmitry Nezvinskyi <dmytro@Dmytros-MacBook.local>
2021-07-03 14:11:43 +0200

    adds lesson 3 - simple express app example

---
+++ 3. express-app/simple-express/routes/users.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+const contacts = [
+  { id: '1', username: 'Felix', surname: 'Brown', email: 'felix@test.com' },
+  { id: '2', username: 'Sonya', surname: 'Redhead', email: 'sonya@test.com' },
+  { id: '3', username: 'Conan', surname: 'Barbarian', email: 'conan@test.com' },
+];
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  res.json(contacts);
+});
+
+// GET contact by id
+router.get('/:id', (req, res, next) => {
+  const { id } = req.params;
+  const contact = contacts.filter(el => el.id === id);
+  res.json(contact);
+});
+module.exports = router;
